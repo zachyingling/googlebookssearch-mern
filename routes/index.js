37485f7f9d401792d5ddbe243d0d4a7340a34fd4
zachary <zpyingling9559@gmail.com>
2020-05-07 23:34:21 -0400

    moving routes from server.js to routes directory

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const router = require("express").Router();
+const key = require("../keys");
+const axios = require("axios").default;
+
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+router.get("/:title", (req, res) => {
+  axios.get("https://www.googleapis.com/books/v1/volumes?q=intitle:" + "&key=" + key)
+  .then((response) => {
+    console.log(response);
+  }).finally(() => {
+    res.send("<h1>Hello node.js</h1>");
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,35 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const axios = require("axios").default;
-const key = require("./keys.js");
 const PORT = process.env.PORT || 3001;
-
-// const db = require("./app/models");
 const app = express();
+const routes = require("./routes");
 
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
+app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/populatedb", { useNewUrlParser: true });
-
-app.get("/", (req, res) => {
-  res.send("Home route");
-});
-
-app.get("/:title", (req, res) => {
-  axios.get("https://www.googleapis.com/books/v1/volumes?q=intitle:&key=" + key)
-  .then((response) => {
-    console.log(response);
-  }).finally(() => {
-    res.send("<h1>Hello node.js</h1>");
-  });
-});
+mongoose.connect((process.env.NODE_ENV || "mongodb://localhost/booksdb"), { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log("App running on port " + PORT + "!");
