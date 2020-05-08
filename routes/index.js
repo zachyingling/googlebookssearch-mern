51150a72d0aa142015877e7e43cdc8dfb2409a69
zachary <zpyingling9559@gmail.com>
2020-05-07 23:37:50 -0400

    removing create react app defaults

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const key = require("../keys");
 const axios = require("axios").default;
 
+// Uses react application when built
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
