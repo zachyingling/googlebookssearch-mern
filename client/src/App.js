a23d3b5ffdf3ec06556ac371f10e58ec42da93fc
zachary <zpyingling9559@gmail.com>
2020-05-08 04:58:34 -0400

    getting the react router to work

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/Nav/index";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
 
 function App () {
     return (
     <Router>
-      <Nav/>
+      <div>
+        <Nav/>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </div>
     </Router>
     );
 }
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Nav() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
-  );
+class Nav extends React.Component {
+  render () {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+              Search
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/saved" className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}>
+                Saved
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Nav;
