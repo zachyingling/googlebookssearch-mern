c1e91fcb953f98c0f7e80c84958ed91296fcc075
zachary <zpyingling9559@gmail.com>
2020-05-12 23:03:40 -0400

    changes to deploy to heroku

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(routes);
 app.use(cors());
 
 // Connect to the Mongo DB
-mongoose.connect((process.env.NODE_ENV || "mongodb://localhost/booksdb"), { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect((process.env.MONGODB_URI || "mongodb://localhost/booksdb"), { useNewUrlParser: true, useUnifiedTopology: true });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.listen(PORT, () => {
   console.log("App running on port " + PORT + "!");
