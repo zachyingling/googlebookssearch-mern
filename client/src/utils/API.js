f6256003ad559b6ec7fa85912f86eda39efa2722
zachary <zpyingling9559@gmail.com>
2020-05-13 05:15:00 -0400

    getting the route to get called with the proper id; still need to set up the mongoose schema and store the book id

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,11 +2,9 @@ import axios from "axios";
 
 export default { 
   apiCall: (title) => {
-    // console.log("title: " + title);
-    // console.log("key: " +process.env.REACT_APP_GOOGLE_KEY);
     return axios.get("/search/" + title + "/" + process.env.REACT_APP_GOOGLE_KEY);
   },
   postRoute: (id) => {
-    return axios.post("/api/saved", id);
+    return axios.post("/api/saved/" + id);
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,14 @@ const db = require("../models");
 const axios = require("axios");
 
 router.route("/search/:title/:key").get((req, res) => {
-  console.log("made it to search;");
   axios.get("https://www.googleapis.com/books/v1/volumes?q=intitle:" + req.params.title + "&key=" + req.params.key).then(response => {
     console.log(response.data.items);
     res.json(response.data.items);
   });
 });
 
-router.route("/api/saved").post((req, res) => {
-  console.log("made it to route");
+router.route("/api/saved/:id").post((req, res) => {
+  console.log("book id: " + req.params.id);
   db.BookID.create();
 });
 
