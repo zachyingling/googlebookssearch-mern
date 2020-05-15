7a10e364ef3854ae8077ffb75a402676f2e55691
zachary <zpyingling9559@gmail.com>
2020-05-14 21:57:06 -0400

    heroku test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 app.use(express.static("public"));
-app.use(cors());
+
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://zachyingling:upenn!2112@ds127646.mlab.com:27646/heroku_753r90kd"|| "mongodb://localhost/booksdb", { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,9 +20,9 @@ mongoose.connect("mongodb://zachyingling:upenn!2112@ds127646.mlab.com:27646/hero
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   // Uses react application when build
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/client", "/build", "/index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "/client", "/build", "/index.html"));
+  // });
 }
 
 app.listen(PORT, () => {
