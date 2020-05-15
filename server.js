2b8fb26369a51c8569d67cfa988b74caf0735778
zachary <zpyingling9559@gmail.com>
2020-05-15 17:22:42 -0400

    another heroku test

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksdb");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   // Uses react application when build
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/client", "/build", "/index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "/client", "/build", "/index.html"));
+  // });
 }
 
 app.use(routes);
