c434af917fc4a648748a8b90eb51e008f9f17482
Zach Yingling <zpyingling9559@gmail.com>
2020-06-12 20:33:22 -0400

    adding dev_google_key environment variable so application can work on local machine and heroku

--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,38 @@ const axios = require("axios");
 router.route("/search/:title").get((req, res) => {
   console.log("heroku key" + process.env.GOOGLE_KEY);
   console.log("REACT key" + process.env.REACT_APP_GOOGLE_KEY);
-  axios.get("https://www.googleapis.com/books/v1/volumes?q=intitle:" + req.params.title + "&key=" + (process.env.GOOGLE_KEY || process.env.REACT_APP_GOOGLE_KEY)).then(response => {
-    res.json(response.data.items);
-  });
+  axios
+    .get(
+      "https://www.googleapis.com/books/v1/volumes?q=intitle:" +
+        req.params.title +
+        "&key=" +
+        (process.env.GOOGLE_KEY ||
+          process.env.REACT_APP_GOOGLE_KEY ||
+          process.env.DEV_GOOGLE_KEY)
+    )
+    .then((response) => {
+      res.json(response.data.items);
+    });
 });
 
 router.route("/api/saved/:id").post((req, res) => {
   let bookID = db.BookID;
 
-  bookID.find({ book_id: req.params.id }).then(response => {
-    if(response.length === 0){
-      bookID.create({ book_id: req.params.id }).then(() => { 
-        res.send(true);
-      }).catch(err => res.send(false));
-    } else {
-      res.send(false);
-    }
-  }).catch(err => res.send(false));
+  bookID
+    .find({ book_id: req.params.id })
+    .then((response) => {
+      if (response.length === 0) {
+        bookID
+          .create({ book_id: req.params.id })
+          .then(() => {
+            res.send(true);
+          })
+          .catch((err) => res.send(false));
+      } else {
+        res.send(false);
+      }
+    })
+    .catch((err) => res.send(false));
 });
 
 module.exports = router;
