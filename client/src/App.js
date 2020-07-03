54d3c97ea2881fa19d7cae519289f47114b54ef6
Zach Yingling <zpyingling9559@gmail.com>
2020-07-02 20:01:55 -0400

    adding hashrouter to see if that will fix heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Route, Switch} from "react-router-dom";
 import Nav from "./components/Nav/index";
 import Saved from "./pages/Saved";
 import Search from "./pages/Search";
@@ -8,16 +8,14 @@ import Jumbotron from "./components/Jumbotron/index";
 class App extends React.Component {
   render () { 
     return (
-      <Router>
-        <div>
-          <Nav path={window.location.pathname}/>
-          <Jumbotron/>
-          <Switch>
-            <Route exact path="/" component={Search} />
-            <Route exact path="/saved" component={Saved} />
-          </Switch>
-        </div>
-      </Router>
+      <div>
+        <Nav path={window.location.pathname}/>
+        <Jumbotron/>
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </div>
     );
   }
 }
