7135ec65dd7b1373cfce83f0a0db95c7bc109539
Zach Yingling <zpyingling9559@gmail.com>
2020-07-02 20:06:40 -0400

    moving hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Route, Switch} from "react-router-dom";
+import { BrowserRouter as HashRouter, Route, Switch} from "react-router-dom";
 import Nav from "./components/Nav/index";
 import Saved from "./pages/Saved";
 import Search from "./pages/Search";
@@ -8,14 +8,16 @@ import Jumbotron from "./components/Jumbotron/index";
 class App extends React.Component {
   render () { 
     return (
-      <div>
-        <Nav path={window.location.pathname}/>
-        <Jumbotron/>
-        <Switch>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/saved" component={Saved} />
-        </Switch>
-      </div>
+      <HashRouter>
+        <div>
+          <Nav path={window.location.pathname}/>
+          <Jumbotron/>
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+          </Switch>
+        </div>
+      </HashRouter>
     );
   }
 }
